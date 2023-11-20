b039544029486eb1928d8eccf15817a790c4e110
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-11-20 08:44:16 +0000

    Architecture, pages, routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './globals.css';
+import { Home } from './_root/pages';
+import SignUpForm from './_auth/forms/SignUpForm';
+import SignInForm from './_auth/forms/SignInForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
 
   return (
-    <div className="text-3xl font-bold text-rose-500">
-      App
+    <div className="flex h-screen">
+      <Routes>
+        {/*  Public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-up" element={<SignUpForm />} />
+          <Route path="/sign-in" element={<SignInForm />} />
+        </Route>
+
+        {/*  Private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
